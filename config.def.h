720c8b88d4e7e339b51761256f2e4b2ec60935a3
jvmf1 <79478101+jvmf1@users.noreply.github.com>
2021-12-06 17:43:18 -0300

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
 
 /*
  * .ms >= 0 : will run every .ms
- * .ms = -1 : will run only on startup
+ * .ms = -1 : will run only on startup and on signal
  * .signal = -1 : will not run on signal
  * .signal = <34...64> : will run cell on signal
  */
